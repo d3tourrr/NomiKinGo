417329d9f8e72366fb6ffbb57f13fea02b3c1290
d3tourrr <wrhoades94@gmail.com>
2024-11-11 15:15:25 -0800

    first draft of rooms API for Nomi

--- Nomi.go
+++ Nomi.go
@@ -7,32 +7,37 @@ import (
     "log"
     "io/ioutil"
     "net/http"
+    "regexp"
 )
 
-var urlComponents = []string {"https://api.nomi.ai/v1/nomis/", "/chat"}
+var UrlComponents map[string][]string
 
-func (nomi *NomiKin) SendNomiMessage (message *string) (string, error) {
-    if len(*message) > 599 {
-        log.Printf("Message too long: %d", len(*message))
-        return fmt.Sprintf("Your message was `%d` characters long, but the maximum message length is 600. Please send a shorter message.", len(*message)), nil
-    }
+func (nomi *NomiKin) Init() {
+    UrlComponents := make(map[string][]string)
+    UrlComponents["SendMessage"] = []string {"https://api.nomi.ai/v1/nomis", "chat"}
+    UrlComponents["RoomCreate"] = []string {"https://api.nomi.ai/v1/rooms"}
+    UrlComponents["RoomSend"] = []string {"https://api.nomi.ai/v1/rooms", "chat"}
+    UrlComponents["RoomReply"] = []string {"https://api.nomi.ai/v1/rooms", "chat/request"}
+}
 
+func (nomi *NomiKin) ApiCall(endpoint string, method string, body interface{}) (map[string]interface{}, error) {
     headers := map[string]string{
-        "Authorization": nomi.ApiKey,
-        "Content-Type": "application/json",
+        "authorization": nomi.ApiKey,
+        "content-type": "application/json",
     }
 
-    bodyMap := map[string]string{
-        "messageText": *message,
+    var jsonBody []byte
+    var err error
+    if body != nil {
+        jsonBody, err = json.Marshal(body)
+        if err != nil {
+            return nil, fmt.Errorf("Error constructing body: %v: ", err)
+        }
     }
-    jsonBody, err := json.Marshal(bodyMap)
-    jsonString := string(jsonBody)
-    log.Printf("Sending message to Nomi %v: %v", nomi.CompanionId, jsonString)
-
-    url := urlComponents[0] + nomi.CompanionId + urlComponents[1]
-    req, err := http.NewRequest("POST", url, bytes.NewBuffer(jsonBody))
+    
+    req, err := http.NewRequest(method, endpoint, bytes.NewBuffer(jsonBody))
     if err != nil {
-        return "", fmt.Errorf("Error reading HTTP request: %v", err)
+        return nil, fmt.Errorf("Error reading HTTP request: %v", err)
     }
 
     req.Header.Set("Authorization", headers["Authorization"])
@@ -41,42 +46,138 @@ func (nomi *NomiKin) SendNomiMessage (message *string) (string, error) {
     client := &http.Client{}
     resp, err := client.Do(req)
     if err != nil {
-        return "", fmt.Errorf("Error making HTTP request: %v", err)
+        return nil, fmt.Errorf("Error making HTTP request: %v", err)
     }
 
     defer resp.Body.Close()
 
-    body, err := ioutil.ReadAll(resp.Body)
+    responseBody, err := ioutil.ReadAll(resp.Body)
     if err != nil {
-        return "", fmt.Errorf("Error reading HTTP response: %v", err)
+        return nil, fmt.Errorf("Error reading HTTP response: %v", err)
     }
 
     if resp.StatusCode != http.StatusOK {
-        returnMsg := ""
-        // Sometimes Nomi responds with an error: {"error":{"type":"NoReply"}}
-        // The Nomi got the message and replied, but the reply wasn't sent back
-        // This is only one kind of error, but it seems to be common enough that we
-        // Should send it back as a message and let them know something happened
         var errorResult map[string]interface{}
-        if err := json.Unmarshal(body, &errorResult); err != nil {
-            return "", fmt.Errorf("Error unmarshalling error response: %v", err)
+        if err := json.Unmarshal(responseBody, &errorResult); err != nil {
+            return nil, fmt.Errorf("Error unmarshalling error response: %v", err)
         }
 
-        if errorMessage, ok := errorResult["error"].(map[string]interface{}); ok {
-            if typeValue, ok := errorMessage["type"].(string); ok {
-                if typeValue == "NoReply" {
-                    log.Print("'NoReply' error - Sending 'Replied but you did not see it' message")
-                    // Send as a reply to the message that triggered the response, helps keep things orderly
-                    returnMsg = "❌ ERROR! ❌\nI got your message and I replied to it, but I took too long to do it so the Nomi API timed out. My response is available in the Nomi app and I have no idea that you didn't get it here. Try saying 'Sorry, I missed what you said when I said...' and send me your message again."
-                }
-            }
-        }
-        return returnMsg, fmt.Errorf("Error response from Nomi API: %v", string(body))
+        return nil, fmt.Errorf("Error response from Nomi API: %v", string(responseBody))
     }
 
     var result map[string]interface{}
-    if err := json.Unmarshal(body, &result); err != nil {
-        return "", fmt.Errorf("Error unmarshalling Nomi %v response: %v", nomi.CompanionId, err)
+    if err := json.Unmarshal(responseBody, &result); err != nil {
+        return nil, fmt.Errorf("Error unmarshalling Nomi %v response: %v", nomi.CompanionId, err)
+    }
+
+    return result, nil
+}
+
+func (nomi *NomiKin) RoomExists(roomName *string) (bool, error) {
+    roomUrl := UrlComponents["RoomCreate"][0] + "/" + *roomName
+    _, err := nomi.ApiCall(roomUrl, "Get", nil)
+    
+    if err != nil {
+        r, _ := regexp.Compile(`RoomNotFound`)
+        if r.MatchString(err.Error()) {
+            log.Printf("Room already exists for Nomi %v: %v", nomi.CompanionId, *roomName)
+            return true, nil
+        } else {
+            log.Printf("Room does not exist for Nomi %v: %v", nomi.CompanionId, *roomName)
+            return false, err
+        }
+    }
+
+    log.Printf("Failed to return an expected response - RoomExists")
+    return false, nil
+}
+
+func (nomi *NomiKin) CreateNomiRoom(name *string, note *string, backchannelingEnabled *bool, nomiUuids []string) (string, error) {
+    roomUrl := UrlComponents["RoomCreate"][0]
+    bodyMap := map[string]interface{}{
+        "name": *name,
+        "note": *note,
+        "backchannelingEnabled": backchannelingEnabled,
+        "nomiUuids": nomiUuids,
+    }
+
+    result, err := nomi.ApiCall(roomUrl, "Post", bodyMap)
+
+    if err != nil {
+        r, _ := regexp.Compile(`RoomNotFound`)
+        if r.MatchString(err.Error()) {
+            log.Printf("Room already exists for Nomi %v: %v", nomi.CompanionId, *name)
+            return *name, nil
+        } else {
+            return "", err
+        }
+    }
+
+    if roomCreateName, ok := result["name"].(string); ok {
+        log.Printf("Created Nomi %v room: %v\n", nomi.CompanionId, roomCreateName)
+        return roomCreateName, nil
+    }
+
+    return "", fmt.Errorf("Failed to return anything meaningful")
+}
+
+func (nomi *NomiKin) SendNomiRoomMessage(message *string, roomId *string) (string, error) {
+    if len(*message) > 599 {
+        log.Printf("Message too long: %d", len(*message))
+        return fmt.Sprintf("Your message was `%d` characters long, but the maximum message length is 600. Please send a shorter message.", len(*message)), nil
+    }
+
+    bodyMap := map[string]string{
+        "messageText": *message,
+    }
+
+    messageSendUrl := UrlComponents["RoomSend"][0] + "/" + *roomId + "/" + UrlComponents["RoomSend"][1]
+    result, err := nomi.ApiCall(messageSendUrl, "Post", bodyMap)
+    if err != nil {
+        log.Printf("Error from API call: %v", err.Error())
+    }
+
+    if replyMessage, ok := result["sentMessage"].(map[string]interface{}); ok {
+        log.Printf("Sent message to room %v: %v\n", roomId, replyMessage)
+        return fmt.Sprintf("Sent message to room %v: %v\n", roomId, replyMessage), nil
+    }
+
+    return "", fmt.Errorf("Failed to return anything meaningful")
+}
+
+func (nomi *NomiKin) RequestNomiRoomReply(roomId *string, nomiId *string) (string, error) {
+    bodyMap := map[string]string{
+        "nomiUuid": *nomiId,
+    }
+
+    messageSendUrl := UrlComponents["RoomReply"][0] + "/" + *roomId + "/" + UrlComponents["RoomReply"][1]
+    result, err := nomi.ApiCall(messageSendUrl, "Post", bodyMap)
+    if err != nil {
+        log.Printf("Error from API call: %v", err.Error())
+    }
+
+    if replyMessage, ok := result["sentMessage"].(map[string]interface{}); ok {
+        log.Printf("Sent message to room %v: %v\n", roomId, replyMessage)
+        return fmt.Sprintf("Sent message to room %v: %v\n", roomId, replyMessage), nil
+    }
+
+    return "", fmt.Errorf("Failed to return anything meaningful")
+}
+
+func (nomi *NomiKin) SendNomiMessage (message *string) (string, error) {
+    if len(*message) > 599 {
+        log.Printf("Message too long: %d", len(*message))
+        return fmt.Sprintf("Your message was `%d` characters long, but the maximum message length is 600. Please send a shorter message.", len(*message)), nil
+    }
+
+    bodyMap := map[string]string{
+        "messageText": *message,
+    }
+
+    messageSendUrl := UrlComponents["SendMessage"][0] + "/" + nomi.CompanionId + "/chat"
+    result, err := nomi.ApiCall(messageSendUrl, "Post", bodyMap)
+    if err != nil {
+        log.Printf("Error from API call: %v", err.Error())
     }
 
     if replyMessage, ok := result["replyMessage"].(map[string]interface{}); ok {
